d119ab8b3aa841b598519f53bdb784814f292f9d
CleveHardcastle <treyhardcastle@gmail.com>
2023-03-20 11:03:52 -0700

    commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 function App() {
   return (
     <div>
-      <div className="w-full flex justify-center">
-        <p className="text-4xl font-bold">
+      <div className="w-full">
+        <p className="text-4xl md:text-5xl text-center font-bold">
           American Dental Association Brushing Recommendations
         </p>
       </div>
-      <div className="w-full flex flex-col md:flex-row mt-10">
-        <div className="w-3/4 m-2 flex items-center text-center">
-          <p className="text-3xl">
+      <div className="w-full flex flex-col md:flex-row mt-10  mb-10">
+        <div className="w-full md:w-3/4 m-0 md:m-2 flex items-center">
+          <p className="text-3xl text-center">
             Brushing your teeth at a 45-degree angle and in an up and down
             motion is a highly recommended technique by dentists worldwide. This
             technique is essential for maintaining good oral hygiene and
@@ -20,16 +20,18 @@ function App() {
           <img src={process.env.PUBLIC_URL + "/images/45.jpg"} />
         </div>
       </div>
-      <div className="flex justify-center">
-        <p className="text-3xl font-bold">Reasons for brushing this way</p>
+      <div className="flex justify-center mb-6">
+        <p className="text-4xl text-center font-bold">
+          Reasons for brushing this way
+        </p>
       </div>
-      <div className="m-2 mt-1">
+      <div className="m-2">
         <div className="w-full">
-          <div className="w-1/2 flex flex-col justify-center">
-            <h2 className="text-center text-2xl font-bold">
-              1. Effective Plaque Removal:
-            </h2>
-            <p className="text-center text-xl">
+          <h2 className="text-center text-2xl font-bold w-full md:w-1/2">
+            1. Effective Plaque Removal:
+          </h2>
+          <div className="flex flex-col md:flex-row place-content-around m-2">
+            <p className="text-center text-xl w-1/2 flex self-center">
               Plaque is a sticky film of bacteria that forms on teeth and gums,
               leading to tooth decay and gum disease. Brushing your teeth at a
               45-degree angle and in an up and down motion is the best way to
@@ -37,10 +39,15 @@ function App() {
               reach into the spaces between the teeth and gums, where plaque
               often accumulates.
             </p>
-            <h2 className="text-center text-2xl font-bold">
-              2. Protects Gum Health:
-            </h2>
-            <p className="text-center text-xl">
+            <div className="w-[300px] flex self-center">
+              <img src={process.env.PUBLIC_URL + "/images/plaque.jpg"} />
+            </div>
+          </div>
+          <h2 className="text-center text-2xl font-bold w-full md:w-1/2">
+            2. Protects Gum Health:
+          </h2>
+          <div className="flex flex-col md:flex-row place-content-around m-2">
+            <p className="text-center text-xl w-1/2 flex self-center">
               Brushing in an up and down motion helps to remove plaque from the
               gum line, preventing gum disease. When you brush at a 45-degree
               angle, the bristles are more effective in removing plaque from the
@@ -48,26 +55,39 @@ function App() {
               your gums than brushing side-to-side, which can cause gum
               recession.
             </p>
-            <h2 className="text-center text-2xl font-bold">
-              3. Prevents Tooth Wear:
-            </h2>
-            <p className="text-center text-xl">
+            <div className="w-[300px] flex self-center">
+              <img src={process.env.PUBLIC_URL + "/images/healthy-gums.jpg"} />
+            </div>
+          </div>
+          <h2 className="text-center text-2xl font-bold w-full md:w-1/2">
+            3. Prevents Tooth Wear:
+          </h2>
+          <div className="flex flex-col md:flex-row place-content-around m-2">
+            <p className="text-center text-xl w-1/2 flex self-center">
               Brushing your teeth with a back-and-forth motion can cause wear on
               the tooth enamel, leading to sensitivity and other dental
               problems. Brushing at a 45-degree angle and in an up and down
               motion is a gentle technique that removes plaque without damaging
               the enamel.
             </p>
-            <h2 className="text-center text-2xl font-bold">
-              4. Freshens Breath:
-            </h2>
-            <p className="text-center text-xl">
+            <div className="w-[300px] flex self-center">
+              <img src={process.env.PUBLIC_URL + "/images/tooth-wear.jpg"} />
+            </div>
+          </div>
+          <h2 className="text-center text-2xl font-bold w-full md:w-1/2">
+            4. Freshens Breath:
+          </h2>
+          <div className="flex flex-col md:flex-row place-content-around m-2">
+            <p className="text-center text-xl w-1/2 flex self-center">
               Brushing at a 45-degree angle and in an up and down motion allows
               you to clean your tongue and the roof of your mouth, where
               bacteria can accumulate and cause bad breath. This technique helps
               to remove odor-causing bacteria, leaving your mouth feeling fresh
               and clean.
             </p>
+            <div className="w-[300px] flex self-center">
+              <img src={process.env.PUBLIC_URL + "/images/breath.jpg"} />
+            </div>
           </div>
         </div>
       </div>
